f3b905dac2ba69d7f6bca421b6cc1dd4284c8643
adam <82415896+adamfil@users.noreply.github.com>
2022-10-24 14:22:51 -0400

    Update bit_utils_test.py
    finished mask and bitmap_has tests

--- test/bit_utils_test.py
+++ test/bit_utils_test.py
@@ -2,15 +2,30 @@ import sys
 sys.path.append("..")
 import bit_utils
 
-#mask tests 
+#mask tests
 print(bit_utils.mask(bytes([0b11111111]), 0, 5) == bytes([0b11111]))
 print(bit_utils.mask(bytes([0b10101010]), 0, 5) == bytes([0b10101]))
 print(bit_utils.mask(bytes([0b10000000]), 0, 5) == bytes([0b10000]))
 print(bit_utils.mask(bytes([0b00010000]), 0, 5) == bytes([0b00010]))
-# fails from here on: print(bit_utils.mask(bytes([0b10000100, 0b10010000]), 0, 9) == bytes([0b100001001])), this is cacuse im not handling > 256 case properly
+print(bit_utils.mask(bytes([0b10000100, 0b10010000]), 0, 9) == bit_utils.bitstring_to_bytes('0b100001001'))
+print(bit_utils.mask(bytes([0b10101010, 0b10101010]), 0, 9) == bit_utils.bitstring_to_bytes('0b101010101'))
+print(bit_utils.mask(bytes([0b10000100, 0b10010000]), 1, 5) == bit_utils.bitstring_to_bytes('0b10010'))
+print(bit_utils.mask(bytes([0b10101010, 0b10101010]), 1, 5) == bit_utils.bitstring_to_bytes('0b01010'))
+print(bit_utils.mask(bytes([0b10000100, 0b10010000]), 2, 5) == bit_utils.bitstring_to_bytes('0b01000'))
+print(bit_utils.mask(bytes([0b10101010, 0b10101010]), 2, 5) == bit_utils.bitstring_to_bytes('0b10101'))
 
 #bitmap_has tests 
+print(not bit_utils.bitmap_has(bytes([0b0]), 0))
+print(not bit_utils.bitmap_has(bytes([0b0]), 1))
+print(bit_utils.bitmap_has(bytes([0b1]), 0))
+print(not bit_utils.bitmap_has(bytes([0b1]), 1))
+print(not bit_utils.bitmap_has(bytes([0b101010]), 2))
 print(bit_utils.bitmap_has(bytes([0b101010]), 3))
+print(not bit_utils.bitmap_has(bytes([0b101010]), 4))
+print(bit_utils.bitmap_has(bytes([0b101010]), 5))
+print(bit_utils.bitmap_has(bytes([0b100000]), 5))
+print(bit_utils.bitmap_has(bytes([0b0100000]), 5))
+print(bit_utils.bitmap_has(bytes([0b00100000]), 5))
 
 #index tests 
 print(bit_utils.index(bytes([0b111111]), 0) == 0)
