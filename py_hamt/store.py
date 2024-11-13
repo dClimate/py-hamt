2a2bdb3bc9058e6e6c9e92762a3f178b1a0625a8
Abid Sikder <41392423+abidsikder@users.noreply.github.com>
2024-11-13 16:52:43 -0500

    Make store an ABC instead of Protocol

--- py_hamt/store.py
+++ py_hamt/store.py
@@ -1,6 +1,13 @@
-from typing import Protocol
+from abc import ABC, abstractmethod
 
 
-class Store(Protocol):
-    async def save(self, node: bytes) -> bytes: ...
-    async def load(self, id: bytes) -> bytes: ...
+class Store(ABC):
+    """This is an Abstract Base Class that represents a storage mechanism the HAMT can use for keeping data."""
+
+    @abstractmethod
+    async def save(self, node: bytes) -> bytes:
+        """Take any set of bytes, save it to the storage mechanism, and return an ID that can be used to retrieve those bytes later."""
+
+    @abstractmethod
+    async def load(self, id: bytes) -> bytes:
+        """Retrieve the bytes based on an ID returned earlier by the save function."""
