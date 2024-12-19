4e8741bcd91905adf17ee577fbb2d055dd37da28
Abid Sikder <41392423+abidsikder@users.noreply.github.com>
2024-12-19 10:50:15 -0500

    Clarify test code

--- performance_tests/cache_with_ipfsstore.py
+++ performance_tests/cache_with_ipfsstore.py
@@ -1,26 +1,24 @@
 from py_hamt import HAMT, IPFSStore
+import time
 
 
 # Run this with varying cache sizes to see the impact on performance of the cache when using IPFSStore()
 def test_and_print_perf():
-    import time
-
-    num_ops = 50
-
     # usual cache size
     hamt = HAMT(store=IPFSStore())
-    start_time = time.time()
+    start_time = time.perf_counter()
+    num_ops = 50
     for key_int in range(num_ops):
         hamt[str(key_int)] = key_int
-    end_time = time.time()
-    op_avg_cache = (end_time - start_time) / 100
+    end_time = time.perf_counter()
+    cache_time = end_time - start_time
 
     # no cache
     hamt = HAMT(store=IPFSStore(), max_cache_size_bytes=0)
-    start_time = time.time()
+    start_time = time.perf_counter()
     for key_int in range(num_ops):
         hamt[str(key_int)] = key_int
-    end_time = time.time()
-    op_avg_no_cache = (end_time - start_time) / 100
+    end_time = time.perf_counter()
+    no_cache_time = end_time - start_time
 
-    print(f"Improvement of {(1 - op_avg_cache / op_avg_no_cache) * 100:.2f}%")
+    print(f"Improvement of {(1 - cache_time / no_cache_time) * 100:.2f}%")
