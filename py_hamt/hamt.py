ebacff01f997755e831775953113f9bca0e039ef
Abid Sikder <41392423+abidsikder@users.noreply.github.com>
2025-02-05 16:55:01 -0500

    add arbitrary transformers

--- py_hamt/hamt.py
+++ py_hamt/hamt.py
@@ -213,6 +213,11 @@ class HAMT(MutableMapping):
     You can modify the read status of a HAMT through the `make_read_only` or `enable_write` functions, so that the HAMT will block on making a change until all mutating operations are done.
     """
 
+    transformer_encode: None | Callable[[str, bytes], bytes]
+    """This function is called to transform the raw bytes of a value, after it is encoded with dag_cbor, which is being inserted right before it gets sent to the Store."""
+    transformer_decode: None | Callable[[str, bytes], bytes]
+    """This function is called to transform the raw bytes of a value, right after it is retrieved from the Store, before it gets dag_cbor decoded into python objects."""
+
     cache: dict[StoreID, Node]
     """@private"""
     max_cache_size_bytes: int
@@ -259,6 +264,8 @@ class HAMT(MutableMapping):
         read_only: bool = False,
         root_node_id: IPLDKind = None,
         max_cache_size_bytes=10_000_000,  # default to 10 megabytes
+        transformer_encode: None | Callable[[str, bytes], bytes] = None,
+        transformer_decode: None | Callable[[str, bytes], bytes] = None,
     ):
         self.store = store
         self.hash_fn = hash_fn
@@ -269,6 +276,9 @@ class HAMT(MutableMapping):
         self.max_bucket_size = 4
         self.read_only = read_only
         self.lock = Lock()
+
+        self.transformer_encode = transformer_encode
+        self.transformer_decode = transformer_decode
 
         if root_node_id is None:
             root_node = Node()
@@ -288,6 +298,8 @@ class HAMT(MutableMapping):
             hash_fn=self.hash_fn,
             read_only=self.read_only,
             root_node_id=self.root_node_id,
+            transformer_encode=self.transformer_encode,
+            transformer_decode=self.transformer_decode,
         )
 
         if not self.read_only:
@@ -349,7 +361,10 @@ class HAMT(MutableMapping):
         if not self.read_only:
             self.lock.acquire(blocking=True)
 
-        val_ptr = self.store.save_raw(dag_cbor.encode(val_to_insert))
+        val = dag_cbor.encode(val_to_insert)
+        if self.transformer_encode is not None:
+            val = self.transformer_encode(key_to_insert, val)
+        val_ptr = self.store.save_raw(val)
 
         node_stack: list[tuple[Link, Node]] = []
         root_node = self.read_node(self.root_node_id)
@@ -545,7 +560,10 @@ class HAMT(MutableMapping):
         if not found_a_result:
             raise KeyError
 
-        return dag_cbor.decode(self.store.load(result_ptr))
+        result_bytes = self.store.load(result_ptr)
+        if self.transformer_decode is not None:
+            result_bytes = self.transformer_decode(key, result_bytes)
+        return dag_cbor.decode(result_bytes)
 
     def __len__(self) -> int:
         key_count = 0
