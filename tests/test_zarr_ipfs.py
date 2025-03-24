577e368c7da7ffc635e974623002a9500606aab7
Abid Sikder <41392423+abidsikder@users.noreply.github.com>
2025-03-24 14:50:45 -0400

    uncomment auth tests

--- tests/test_zarr_ipfs.py
+++ tests/test_zarr_ipfs.py
@@ -211,41 +211,41 @@ def test_encryption(random_zarr_dataset: tuple[str, xr.Dataset]):
         ds.precip.sum()
 
 
-# # This test assumes the other zarr ipfs tests are working fine, so if other things are breaking check those first
-# def test_authenticated_gateway(random_zarr_dataset: tuple[str, xr.Dataset]):
-#     _, test_ds = random_zarr_dataset
+# This test assumes the other zarr ipfs tests are working fine, so if other things are breaking check those first
+def test_authenticated_gateway(random_zarr_dataset: tuple[str, xr.Dataset]):
+    _, test_ds = random_zarr_dataset
 
-#     def write_and_check(store: IPFSStore) -> bool:
-#         try:
-#             store.rpc_uri_stem = "http://127.0.0.1:5002"  # 5002 is the port configured in the run-checks.yaml actions file for nginx to serve the proxy on
-#             hamt = HAMT(store=store)
-#             test_ds.to_zarr(store=hamt, mode="w")
-#             loaded_ds = xr.open_zarr(store=hamt)
-#             xr.testing.assert_identical(test_ds, loaded_ds)
-#             return True
-#         except Exception as _:
-#             return False
+    def write_and_check(store: IPFSStore) -> bool:
+        try:
+            store.rpc_uri_stem = "http://127.0.0.1:5002"  # 5002 is the port configured in the run-checks.yaml actions file for nginx to serve the proxy on
+            hamt = HAMT(store=store)
+            test_ds.to_zarr(store=hamt, mode="w")
+            loaded_ds = xr.open_zarr(store=hamt)
+            xr.testing.assert_identical(test_ds, loaded_ds)
+            return True
+        except Exception as _:
+            return False
 
-#     # Test with API Key
-#     api_key_store = IPFSStore(api_key="test")
-#     assert write_and_check(api_key_store)
+    # Test with API Key
+    api_key_store = IPFSStore(api_key="test")
+    assert write_and_check(api_key_store)
 
-#     # Test that wrong API Key fails
-#     bad_api_key_store = IPFSStore(api_key="badKey")
-#     assert not write_and_check(bad_api_key_store)
+    # Test that wrong API Key fails
+    bad_api_key_store = IPFSStore(api_key="badKey")
+    assert not write_and_check(bad_api_key_store)
 
-#     # Test just bearer token
-#     bearer_ipfs_store = IPFSStore(bearer_token="test")
-#     assert write_and_check(bearer_ipfs_store)
+    # Test just bearer token
+    bearer_ipfs_store = IPFSStore(bearer_token="test")
+    assert write_and_check(bearer_ipfs_store)
 
-#     # Test with wrong bearer
-#     bad_bearer_store = IPFSStore(bearer_token="wrongBearer")
-#     assert not write_and_check(bad_bearer_store)
+    # Test with wrong bearer
+    bad_bearer_store = IPFSStore(bearer_token="wrongBearer")
+    assert not write_and_check(bad_bearer_store)
 
-#     # Test with just basic auth
-#     basic_auth_store = IPFSStore(basic_auth=("test", "test"))
-#     assert write_and_check(basic_auth_store)
+    # Test with just basic auth
+    basic_auth_store = IPFSStore(basic_auth=("test", "test"))
+    assert write_and_check(basic_auth_store)
 
-#     # Test with wrong basic auth
-#     bad_basic_auth_store = IPFSStore(basic_auth=("wrong", "wrong"))
-#     assert not write_and_check(bad_basic_auth_store)
+    # Test with wrong basic auth
+    bad_basic_auth_store = IPFSStore(basic_auth=("wrong", "wrong"))
+    assert not write_and_check(bad_basic_auth_store)
