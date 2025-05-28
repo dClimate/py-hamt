9560c26e3cad0830b6258301170306c124c2c3df
TheGreatAlgo <37487508+TheGreatAlgo@users.noreply.github.com>
2025-05-28 11:27:35 -0400

    fix: simple encrypted zarr

--- py_hamt/__init__.py
+++ py_hamt/__init__.py
@@ -1,6 +1,7 @@
 from .hamt import HAMT, blake3_hashfn
 from .store import ContentAddressedStore, KuboCAS
 from .zarr_hamt_store import ZarrHAMTStore
+from .encryption_hamt_store import SimpleEncryptedZarrHAMTStore
 
 __all__ = [
     "blake3_hashfn",
@@ -8,4 +9,5 @@ __all__ = [
     "ContentAddressedStore",
     "KuboCAS",
     "ZarrHAMTStore",
+    "SimpleEncryptedZarrHAMTStore",
 ]
