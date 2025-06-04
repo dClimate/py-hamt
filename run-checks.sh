1632dca793541d028179349cfd96e8754e50c554
Faolain <Faolain@users.noreply.github.com>
2025-06-04 00:30:50 -0400

    refactor: ensure run-checks will check all files not just those that are staged

--- run-checks.sh
+++ run-checks.sh
@@ -7,4 +7,4 @@ uv run pytest --ipfs --cov=py_hamt tests/
 uv run coverage report --fail-under=100 --show-missing
 
 # Check for linting, formatting, and type checking using the pre-commit hooks found in .pre-commit-config.yaml
-uv run pre-commit
+uv run pre-commit run --all-files --show-diff-on-failure
